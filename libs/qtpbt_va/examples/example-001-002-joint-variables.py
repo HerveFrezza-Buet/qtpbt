a817c007343273a456d579ab5ea3a0a7fa5e5092
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2022-09-02 11:23:56 +0200

    updt

--- libs/qtpbt_va/examples/example-001-002-joint-variables.py
+++ libs/qtpbt_va/examples/example-001-002-joint-variables.py
@@ -4,6 +4,8 @@ import numpy as np
 import qtpbt_va as va
 import matplotlib.pyplot as plt
 
+offset = .05
+    
 Xs  = np.array([0.00, 0.01, 0.05, 0.10, 0.20, 0.50])
 Ys  = np.array([0.00, 0.10, 0.30, 0.35, 0.40, 0.50])
 XXs = 1.0 - np.flip(Xs[:-1])
@@ -24,14 +26,16 @@ U = va.sampler.Uniform()
 I = va.sampler.Apply(U, fI, vectorized = True)
 J = va.sampler.Apply(U, fJ, vectorized = True)
 
+UU      = va.sampler.Nuplet(U, 2)
+IJindep = va.sampler.Apply(UU, lambda u1u2 : (fI(u1u2[0]), fJ(u1u2[1])))
+
 def test_001():
-    fig = plt.figure(figsize = (10, 5))
     nb_samples = 300
     dataI = I(nb_samples)
-    dataJ = I(nb_samples)
+    dataJ = J(nb_samples)
 
     fig = plt.figure(figsize = (10, 5))
-    gs  = fig.add_gridspec(2, 1,
+    gs  = fig.add_gridspec(1, 2,
                            width_ratios  = [2, 2],
                            height_ratios = [1],
                            wspace=0.1, hspace=0.1)
@@ -41,12 +45,37 @@ def test_001():
     ax.set_title('Random variable I')
     ax.set_xlim((0.0, 1.0))
     ax.set_ylim((0.0, 1.0))
-    va.plot.spline_1d(ax, fI, 0, 1, 200)
-    ax.scatter(from_samples, np.zeros(nb_samples) + offset, alpha = .1)
-    ax.scatter(np.ones(nb_samples) - offset, samples,       alpha = .1)
-
+    va.plot.function_1d(ax, fI, 0, 1, 200)
+    ax.scatter(dataI['from_samples'], np.zeros(nb_samples) + offset, alpha = .05)
+    ax.scatter(np.ones(nb_samples) - offset, dataI['samples'],       alpha = .05)
     
+    ax = fig.add_subplot(gs[0, 1])
+    ax.set_aspect('equal')
+    ax.set_title('Random variable J')
+    ax.set_xlim((0.0, 1.0))
+    ax.set_ylim((0.0, 1.0))
+    va.plot.function_1d(ax, fJ, 0, 1, 200)
+    ax.scatter(dataJ['from_samples'], np.zeros(nb_samples) + offset, alpha = .05)
+    ax.scatter(np.ones(nb_samples) - offset, dataJ['samples'],       alpha = .05)
 
+    plt.show()
+
+def test_002():
+    nb_samples = 5000
+    samples = IJindep(nb_samples)['samples']
+    
+    fig = plt.figure(figsize = (10,10))
+    gs  = fig.add_gridspec(2, 2,
+                           width_ratios  = [2, 1],
+                           height_ratios = [2, 1],
+                           wspace=0.2, hspace=0.2)
+
+    va.plot.joint(fig.add_subplot(gs[0, 0]),
+                  fig.add_subplot(gs[1, 0]),
+                  fig.add_subplot(gs[0, 1]),
+                  'I', 'J', samples, offset=offset)
+    
+    plt.show()
     
     
 
--- libs/qtpbt_va/qtpbt_va/plot.py
+++ libs/qtpbt_va/qtpbt_va/plot.py
@@ -1,22 +1,51 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from . import estimator
 
-def function_1d(ax, f, inf, sup, nb, vectorized = True, orientation = 'xy'):
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+def function_1d(ax, f, inf, sup, nb, vectorized = True, orientation = 'xy', color=colors[0]):
     Xs = np.linspace(inf, sup, nb, endpoint=True)
     if vectorized:
         Ys = f(Xs)
     else:
         Ys = np.array([f(x) for x in Xs])
     if orientation == 'xy':
-        ax.plot(Xs, Ys)
+        ax.plot(Xs, Ys, c=color)
     else:
-        ax.plot(Ys, Xs)
+        ax.plot(Ys, Xs, c=color)
     
 
 
-def spline_1d(ax, f, inf, sup, nb, orientation = 'xy'):
+def spline_1d(ax, f, inf, sup, nb, orientation = 'xy', color=colors[0]):
     function_1d(ax, f, inf, sup, nb, orientation)
     if orientation == 'xy':
-        ax.scatter(f.Xs, f.Ys, c='k')
+        ax.scatter(f.Xs, f.Ys, c=color)
     else:
-        ax.scatter(f.Ys, f.Xs, c='k')
+        ax.scatter(f.Ys, f.Xs, c=color)
+
+def joint(ax_ij, ax_i, ax_j, i_name, j_name, samples,  bin_width=.05, degree=2, offset=.1,
+          sample_color = colors[1], density_color = colors[0]):
+    nb_samples = len(samples)
+    ax_ij.set_aspect('equal')
+    ax_ij.set_title(f'({i_name}, {j_name}), joint')
+    ax_ij.set_xlim((0.0, 1.0))
+    ax_ij.set_ylim((0.0, 1.0))
+    ax_ij.scatter(samples[...,0], samples[..., 1], alpha = .1, color = sample_color)
+
+    ax_j.set_aspect('equal')
+    ax_j.set_title(f'{j_name}, marginalization')
+    ax_j.set_xlim((0.0, 0.5))
+    ax_j.set_ylim((0.0, 1.0))
+    pJ  = estimator.density_1d(samples[..., 1], bin_width=.05, degree=2)
+    ax_j.scatter(np.zeros(nb_samples) + offset, samples[..., 1], alpha = 0.01, color = sample_color)
+    function_1d(ax_j, pJ, 0, 1, 200, orientation = 'yx', color = density_color)
+
+    ax_i.set_aspect('equal')
+    ax_i.set_title(f'{i_name}, marginalization')
+    ax_i.set_xlim((0.0, 1.0))
+    ax_i.set_ylim((0.0, 0.5))
+    pI  = estimator.density_1d(samples[..., 0], bin_width=.05, degree=2)
+    ax_i.scatter(samples[..., 0], np.zeros(nb_samples) + offset, alpha = 0.01, color = sample_color)
+    function_1d(ax_i, pI, 0, 1, 200, orientation = 'xy', color = density_color)
     
